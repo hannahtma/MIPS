76a8c56c46bcb38d828bec0852df04bcde3ea9bf
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-08-17 12:21:29 +0800

    Update task3.asm

--- task3.asm
+++ task3.asm
@@ -2,93 +2,126 @@
 
     .data
 
-my_list: .word 3 10 14 16
-hulk_power: .word 15
-front_output: .asciiz "Hulk smashed "
-back_output: .asciiz " people"
-
-smash_count: .word 0
-i: .word 0
-temp: .word 0
-diff: .word 0
 hulk_smash: .asciiz "Hulk SMASH! >:("
 hulk_sad: .asciiz "Hulk Sad :("
 
 newline: .asciiz "\n"
 
     .text
+smash_or_sad:	# smash_or_sad function
 
-j main
+                    # memory diagram
+            ##################################
+            # smash_count	 -   -8($fp) #
+            # i                  -   -4($fp) #
+            # fp                 -    0($fp) #
+            # ra                 -   +4($fp) #
+            # the_list           -   +8($fp) #
+            # hulk_power         -  +12($fp) #
+            ##################################
 
-# smash_or_sad function
-smash_or_sad:
-	lw $t0, size
-	sw $t0, len_my_list
+		addi $sp, $sp, -4
+		sw $ra, ($sp)
+		addi $sp, $sp, -4
+		sw $fp, ($sp)
+		addi $fp, $sp, 0
+		addi $sp, $sp, -16
+		
+		lw $t0, -8($fp)
+		sw $0, -8($fp)
+		
+		lw $t0, -4($fp)
+		sw $0, -4($fp)
+		
+		lw $t0, -4($fp)
+		lw $t1, +8($fp)
+		slt $t2, $t0, $t1 # If t0 < t1, then t2 = 1
+		beq $t2, $0, end
+		
+		arrayloop:
+		lw $t0, -4($fp)
+		addi $t0, $t0, 4
+		sw $t0, -4($fp)
+		lw $t1, +12($fp)
+		sub $t2, $t1, $t0 # $t2 = $t1 - $t0
+		slt $t3, $t2, $0 # If t2 < 0, then t3 = 1
+		bne $t3 $0, equalsone # Branch if t3 = 0 (t3 >= 0)
+		
+		# Print "Hulk SMASH! >:("
+		la $a0, hulk_smash
+		addi $v0, $0, 4
+		syscall
+		la $a0, newline
+		addi $v0, $0, 4
+		syscall
+		
+		lw $t0, -8($fp)
+		addi $t0, $t0, 1
+		sw $t0, -8($fp)
+		
+		equalsone:
+		# Print "Hulk Sad :("
+		la $a0, hulk_sad
+		addi $v0, $0, 4
+		syscall
+		la $a0, newline
+		addi $v0, $0, 4
+		syscall
+		
+		lw $t0, -4($fp)
+		addi $t0, $t0, 1
+		sw $t0, -4($fp)
+		
+		lw $t0, -8($fp)
+		mflo $v0
+		
+		addi $sp, $sp, 16
+		lw $fp, ($sp)
+		addi $sp, $sp, +4
+		lw $ra, ($sp)
+		addi $sp, $sp, +4
 
-	lw $t0, i
-	lw $t1, len_my_list
-	slt $t2, $t0, $t1 # if t0 < t1, then t2 = 1
-	beq $t2, $0, end # if t2 = 0, branch to end
-
-	lw $t0, i
-	sll $t0, $t0, 2
-	lw $t1, my_list
-	addi $t0, $t1, 0
-	lw $t2, 4($t1)
-	sw $t2, temp
-
-	lw $t0, temp
-	lw $t1, hulk_smash
-	sub $t2, $t0, $t1
-	sw $t2, diff
-
-	lw $t0, diff
-	slt $t1, $t0, $0 # if t0 < 0, then t1 = 1
-	bne $t0, $0, hulksmash
-
-	la $a0, hulk_sad
-	addi $v0, $0, 4
-	syscall
-	la $a0, newline
-	addi $v0, $0, 4
-	syscall
-
-	hulksmash:
-	la $a0, hulk_smash
-	addi $v0, $0, 4
-	syscall
-	la $a0, newline
-	addi $v0, $0, 4
-	syscall
-
-	lw $t0, smash_count
-	addi $t1, $t0, 1
-	sw $t1, smash_count
-	
-	# i += 1
-	lw $t0, i
-	addi $t0, $t0, 1
-	sw $t0, i
-	
-	j smash_or_sad
+		# End program
+		end:
+		jr $ra
 
 # Main
-main:
-	la $a0, front_output
-	addi $v0, $0, 4
-	syscall
+main:	
 
-	lw $a0, smash_count
-	addi $v0, $0, 1
-	syscall
-	
-	la $a0, back_output
-	addi $v0, $0, 4
-	syscall
-	
-	j smash_or_sad
+                    # memory diagram
+            ##################################
+            # hulk_power	 -   -8($fp) #
+            # list_of_numbers    -   -4($fp) #
+            # fp                 -    0($fp) #
+            ##################################
 
-# End program
-end:
-	addi $v0, $0, 10
+	addi $sp, $sp, -4
+	sw $fp, ($sp)
+	addi $fp, $sp, 0
+	addi $sp, $sp, -8
+	
+	addi $a0, $0, 16
+	addi $v0, $0, 9
 	syscall
+	sw $v0, -4($fp)
+	
+	lw $t0, -4($fp)
+	
+	addi $t1, $0, 3
+	sw $t1, ($t0)
+	
+	addi $t1, $0, 10
+	sw $t1, 4($t0)
+	
+	addi $t1, $0, 14
+	sw $t1, 8($t0)
+	
+	addi $t1, $0, 16
+	sw $t1, 12($t0)
+	
+	lw $t0, -8($fp)
+	addi $t0, $0, 15
+	sw $t0, -8($fp)
+	
+	jal smash_or_sad
+	
