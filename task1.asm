dafbf6f50870243cb3e9fc4c1b269a0bbe1116c8
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-08-20 16:29:17 +0800

    Update task1.asm

--- task1.asm
+++ task1.asm
@@ -170,9 +170,9 @@ finish: # total_cost = total_cost + (consumption*tier_one_price)
         mflo $t0 # t0 = t0 // t1
         sw $t0, dollars # dollars = 
 
-        #lw $t0, total_bill
-        #addi $t1, $0, 100
-        #div $t0, $t1
+        lw $t0, total_bill
+        addi $t1, $0, 100
+        div $t0, $t1
         mfhi $t1
         sw $t1, cents
 
