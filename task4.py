f7041cf3cdb27a10a7e9508beefbc5b6003c1f82
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-08-21 17:54:03 +0800

    Update task4.py

--- task4.py
+++ task4.py
@@ -10,7 +10,7 @@ def insertion_sort(the_list: List[T]):
     length = len(the_list)
     # Goes through the_list starting from i = 1
     for i in range(1, length):
-        # Stores the_list[i] into another variable key so the value won't be lost
+        # Stores the_list[i] into another variable key so the value of the_list[i] won't be lost
         key = the_list[i]
         # Stores the value of i-1 into j to compare two consecutive elements
         j = i-1
@@ -21,7 +21,7 @@ def insertion_sort(the_list: List[T]):
                 the_list[j + 1] = the_list[j]
                 # Subtract 1 from j to refer to the element before
                 j -= 1
-        # Replace the (j+1)th element with the key value
+        # Replace the (j+1)th element with the element in key
         the_list[j + 1] = key
 
 def main() -> None:
